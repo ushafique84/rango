0f21befad5b6ba49405b31d16c9ad0218f254a2f
Usman Shafique <ushafique84@yahoo.com>
2014-07-19 17:12:21 -0700

    with updated database to include views and likes for categories and updates to population script. Also, set up top 5 categories in front page

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -2,9 +2,16 @@ from django.db import models
 
 class Category(models.Model):
 	name = models.CharField(max_length=128, unique=True)
+	views = models.IntegerField(default=0)
+	likes = models.IntegerField(default=0)
 
 	def __unicode__(self):
 		return self.name
+	
+	#provide the model metadata using an inner class
+	#one is to set the correct plural for Category
+	class Meta:
+		verbose_name_plural = "Categories"
 
 class Page(models.Model):
 	category = models.ForeignKey(Category)
