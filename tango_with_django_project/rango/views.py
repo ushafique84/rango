ae9f02b4a3b593d9cf6e6247c473c2d98c072eff
Usman Shafique <ushafique84@yahoo.com>
2014-07-24 12:28:30 -0700

    added bootstrap css and img files. Also added jQuery 2.1.1. Set up Rango to use fluid bootstrap template

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.template import RequestContext
@@ -12,9 +13,10 @@ def index(request):
 	#Request the context of the request.
 	#The context contains information such as the client's machine details, for example
 	context = RequestContext(request)
+	
 	#Construct a dictionary to pass to the template engine as its context.
 	#Note the key boldmessage is the same as {{ boldmessage }} in the template!
-	category_list = Category.objects.order_by('-likes')[:5]
+	category_list = Category.objects.all()
 	page_list = Page.objects.order_by('-views')[:5]
 	context_dict = {'categories': category_list,
 			'pages': page_list,
@@ -25,18 +27,35 @@ def index(request):
 	for category in category_list:
 		category.url = encode_url(category)
 
-	for page in page_list:
-		page.url 
+	if request.session.get('last_visit'):
+		#The session has a value for the last visit
+		last_visit_time = request.session.get('last_visit')
+		visits = request.session.get('visits', 0)
+
+		if (datetime.now() - datetime.strptime(last_visit_time[:19], "%Y-%m-%d %H:%M:%S")).days > 0:
+			request.session['visits'] = visits + 1
+			request.session['last_visit'] = str(datetime.now())
+	else:
+		#The get returns None, and the session does not have a value for the last visit.
+		request.session['last_visit'] = str(datetime.now())
+		request.session['visits'] = 1
 	
-	#Return a rendered response to send to the client.
-	#We make use of the shortcut function to make our lives easier.
-	#Note that the first parameter is the template we wish to use.
 	return render_to_response('rango/index.html', context_dict, context)
 
 def about(request):
 	context = RequestContext(request)
-	#return HttpResponse("Rango Says: Here is the about page.")
-	return render_to_response('rango/about.html', {}, context)
+	
+	#If the visits session variable exists, take it and use it
+	#If it doesn't, set the count to zero
+	if request.session.get('visits'):
+		count = request.session.get('visits')
+	else:
+		count = 0
+	
+	context_dict = {}
+	context_dict['visits'] = count
+
+	return render_to_response('rango/about.html', context_dict, context)
 
 def decode_url(url):
 	return url.replace('_', ' ')
